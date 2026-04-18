d21fb7f6fc4f93ac0f89f6ec188d4257458be7b6
huhaochi221B <huhaochi221B@outlook.com>
2026-04-18 18:56:56 +0800

    保存当前工作

---
+++ src/main/java/com/hhc/hhcaiagent/controller/ExampleController.java
@@ -0,0 +1,14 @@
+package com.hhc.hhcaiagent.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/example")
+public class ExampleController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello!";
+    }
+}
